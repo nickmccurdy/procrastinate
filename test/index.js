f378057fa2c286a19cd222f29806db398c02486f
Nicolas McCurdy <thenickperson@gmail.com>
2015-04-14 21:34:33 -0400

    Start implementing specs

--- test/index.js
+++ test/index.js
@@ -1,9 +1,21 @@
+var assert = require('assert');
+var fs = require('fs');
+var procrastinate = require('..');
+
 describe('Procrastinate', function () {
   context('with an empty input', function () {
-    it('returns an empty string');
+    it('returns an empty string', function () {
+      assert.equal(procrastinate(''), '');
+    });
   });
 
   context('with an input representing pending Mocha specs', function () {
-    it('returns pending Mocha specs');
+    var fileOptions = { encoding: 'utf-8' };
+    var input  = fs.readFileSync('test/data/input.txt', fileOptions);
+    var output = fs.readFileSync('test/data/output.js', fileOptions);
+
+    it('returns pending Mocha specs', function () {
+      assert.equal(procrastinate(input), output);
+    });
   });
 });
