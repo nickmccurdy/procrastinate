9ec5a7993b7a7a3c90eaf300cb60ff526a361811
Nicolas McCurdy <thenickperson@gmail.com>
2015-04-14 21:18:24 -0400

    Add basic pending specs

---
+++ test/index.js
@@ -0,0 +1,9 @@
+describe('Procrastinate', function () {
+  context('with an empty input', function () {
+    it('returns an empty string');
+  });
+
+  context('with an input representing pending Mocha specs', function () {
+    it('returns pending Mocha specs');
+  });
+});
