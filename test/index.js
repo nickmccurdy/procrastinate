6f54d5fd69d12e4e3f84c93312c58fa2d28c9916
Nicolas McCurdy <thenickperson@gmail.com>
2016-06-18 16:16:42 -0400

    Assert with strict equality

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ var procrastinate = require('..');
 describe('procrastinate()', function () {
   context('with an empty input', function () {
     it('returns an empty string', function () {
-      assert.equal(procrastinate('mocha', ''), '');
+      assert.strictEqual(procrastinate('mocha', ''), '');
     });
   });
 
@@ -24,7 +24,7 @@ describe('procrastinate()', function () {
     var output = getData('mocha_output.js');
 
     it('returns pending Mocha specs', function () {
-      assert.equal(procrastinate('mocha', input), output);
+      assert.strictEqual(procrastinate('mocha', input), output);
     });
   });
 
@@ -32,13 +32,13 @@ describe('procrastinate()', function () {
     var output = getData('rspec_output.rb');
 
     it('returns pending RSpec specs', function () {
-      assert.equal(procrastinate('rspec', input), output);
+      assert.strictEqual(procrastinate('rspec', input), output);
     });
   });
 
   xcontext('with an input containing empty lines', function () {
     it('returns pending specs with empty lines removed', function () {
-      assert.equal(procrastinate('rspec', 'does this\n\ndoes that'),
+      assert.strictEqual(procrastinate('rspec', 'does this\n\ndoes that'),
         "it 'does this'\nit 'does that'");
     });
   });
@@ -65,27 +65,27 @@ describe('procrastinate', function () {
     context('with a line of type "suite"', function () {
       xcontext('with no indents or text', function () {
         it('returns null', function () {
-          assert.equal(procrastinate.format('rspec', '', 'suite'), null);
+          assert.strictEqual(procrastinate.format('rspec', '', 'suite'), null);
         });
       });
 
       context('with no indents', function () {
         it('returns the start of a suite', function () {
-          assert.equal(procrastinate.format('rspec', 'example', 'suite'),
+          assert.strictEqual(procrastinate.format('rspec', 'example', 'suite'),
             "describe 'example' do");
         });
       });
 
       context('with one indent', function () {
         it('returns the start of a suite with one indent', function () {
-          assert.equal(procrastinate.format('rspec', '  example', 'suite'),
+          assert.strictEqual(procrastinate.format('rspec', '  example', 'suite'),
             "  describe 'example' do");
         });
       });
 
       context('with two indents', function () {
         it('returns the start of a suite with two indents', function () {
-          assert.equal(procrastinate.format('rspec', '    example', 'suite'),
+          assert.strictEqual(procrastinate.format('rspec', '    example', 'suite'),
             "    describe 'example' do");
         });
       });
@@ -94,25 +94,25 @@ describe('procrastinate', function () {
     context('with a line of type "test"', function () {
       xcontext('with no indents or text', function () {
         it('returns null', function () {
-          assert.equal(procrastinate.format('rspec', '', 'test'), null);
+          assert.strictEqual(procrastinate.format('rspec', '', 'test'), null);
         });
       });
 
       context('with no indents', function () {
         it('returns the start of a test', function () {
-          assert.equal(procrastinate.format('rspec', 'example', 'test'), "it 'example'");
+          assert.strictEqual(procrastinate.format('rspec', 'example', 'test'), "it 'example'");
         });
       });
 
       context('with one indent', function () {
         it('returns the start of a test with one indent', function () {
-          assert.equal(procrastinate.format('rspec', '  example', 'test'), "  it 'example'");
+          assert.strictEqual(procrastinate.format('rspec', '  example', 'test'), "  it 'example'");
         });
       });
 
       context('with two indents', function () {
         it('returns the start of a test with two indents', function () {
-          assert.equal(procrastinate.format('rspec', '    example', 'test'), "    it 'example'");
+          assert.strictEqual(procrastinate.format('rspec', '    example', 'test'), "    it 'example'");
         });
       });
     });
@@ -120,25 +120,25 @@ describe('procrastinate', function () {
     context('with a line of type "end"', function () {
       xcontext('with no indents or text', function () {
         it('returns null', function () {
-          assert.equal(procrastinate.format('rspec', '', 'end'), null);
+          assert.strictEqual(procrastinate.format('rspec', '', 'end'), null);
         });
       });
 
       context('with no indents', function () {
         it('returns the end of a suite or test', function () {
-          assert.equal(procrastinate.format('rspec', 'example', 'end'), 'end');
+          assert.strictEqual(procrastinate.format('rspec', 'example', 'end'), 'end');
         });
       });
 
       context('with one indent', function () {
         it('returns the end of a suite or test with one indent', function () {
-          assert.equal(procrastinate.format('rspec', '  example', 'end'), '  end');
+          assert.strictEqual(procrastinate.format('rspec', '  example', 'end'), '  end');
         });
       });
 
       context('with two indents', function () {
         it('returns the end of a suite or test with two indents', function () {
-          assert.equal(procrastinate.format('rspec', '    example', 'end'), '    end');
+          assert.strictEqual(procrastinate.format('rspec', '    example', 'end'), '    end');
         });
       });
     });
@@ -149,7 +149,7 @@ describe('procrastinate', function () {
       assert(Array.isArray(procrastinate.formatters));
 
       procrastinate.formatters.forEach(function (formatter) {
-        assert.equal(typeof formatter, 'string');
+        assert.strictEqual(typeof formatter, 'string');
         assert(/^\w+$/.test(formatter));
       });
     });
@@ -158,25 +158,25 @@ describe('procrastinate', function () {
   describe('.getIndentLength()', function () {
     context('given an empty line', function () {
       it('returns 0', function () {
-        assert.equal(procrastinate.getIndentLength(''), 0);
+        assert.strictEqual(procrastinate.getIndentLength(''), 0);
       });
     });
 
     context('given a line with no indents', function () {
       it('returns 0', function () {
-        assert.equal(procrastinate.getIndentLength('example'), 0);
+        assert.strictEqual(procrastinate.getIndentLength('example'), 0);
       });
     });
 
     context('given a line with one indent', function () {
       it('returns 1', function () {
-        assert.equal(procrastinate.getIndentLength('  example'), 1);
+        assert.strictEqual(procrastinate.getIndentLength('  example'), 1);
       });
     });
 
     context('given a line with two indents', function () {
       it('returns 2', function () {
-        assert.equal(procrastinate.getIndentLength('    example'), 2);
+        assert.strictEqual(procrastinate.getIndentLength('    example'), 2);
       });
     });
   });
@@ -202,25 +202,25 @@ describe('procrastinate', function () {
   describe('.unindent()', function () {
     context('given an empty line', function () {
       it('returns an empty String', function () {
-        assert.equal(procrastinate.unindent(''), '');
+        assert.strictEqual(procrastinate.unindent(''), '');
       });
     });
 
     context('given a line with no indents', function () {
       it('returns the same line', function () {
-        assert.equal(procrastinate.unindent('example'), 'example');
+        assert.strictEqual(procrastinate.unindent('example'), 'example');
       });
     });
 
     context('given a line with one indent', function () {
       it('returns the same line with no indents', function () {
-        assert.equal(procrastinate.unindent('  example'), 'example');
+        assert.strictEqual(procrastinate.unindent('  example'), 'example');
       });
     });
 
     context('given a line with two indents', function () {
       it('returns the same line with only one indent', function () {
-        assert.equal(procrastinate.unindent('    example'), '  example');
+        assert.strictEqual(procrastinate.unindent('    example'), '  example');
       });
     });
   });
